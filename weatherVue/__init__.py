802bafb1c224600ca23bfa50464def1ea27d65d6
LukeHiura <87911844+lhiura001@users.noreply.github.com>
2023-04-17 22:25:43 -0700

    Update __init__.py

--- weatherVue/__init__.py
+++ weatherVue/__init__.py
@@ -8,7 +8,6 @@ from flask_mail import Mail
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-api_g_maps = 'AIzaSyAaccF2c64kfnoJFy8G4RmhYGI7ohOKrE4'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
